274c427d19d200fac8af0fb354179b8a56b309e0
jaider328 <jaiderb@nycstudents.net>
2025-11-14 10:33:33 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+from myfunctions import *
+
+for times in range(10):
+    triangle()
+  
+
+   
+
---
+++ myfunctions.py
@@ -0,0 +1,22 @@
+import turtle
+bob=turtle.Turtle()
+
+#triangle
+def triangle():
+    for times in range(3):
+        bob.forward(100)
+        bob.left(120)
+
+#square
+def square():
+    for times in range(4):
+           bob.forward(100)
+           bob.left(100)
+        
+#pentagon
+def pentagon():
+    for times in range(5):
+        bob.forward(100)
+        bob.left(72)
+        
+    
