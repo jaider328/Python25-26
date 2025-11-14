e08f596926d3197e2afbec1add6c2edbb1aa545d
jaider328 <jaiderb@nycstudents.net>
2025-11-14 10:36:07 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,27 @@
+import turtle
 from myfunctions import *
+import random
 
-for times in range(10):
-    triangle()
-  
+# --- Setup ---
+screen_height = screen.window_height()
+square_size = 30
+rows = screen_height // square_size  # integer division to get whole squares
+cols = screen_width // square_size
+offset = 0 # variable for the offest in the x,y
 
-   
+# --- Colors ---
+colors = ["red", "orange", "yellow", "green", "blue", "purple", "white"]
 
+# --- Draw the Pattern ---
+for row in range(rows):
+    for col in range(cols):
+        x = col * square_size - screen_width // 2 + square_size//2  # Centering
+        y = row * square_size - screen_height // 2 + square_size//2
+        t.penup()
+        t.goto(x, y)
+        t.pendown()
+        color1 = random.choice(colors)
+        color2 = random.choice(colors)
+        mf.draw_diamond(t, square_size, color1, color2, offset)
+        offset += 2 # add a small offset
+screen.mainloop()  # Keep the window open        
--- myfunctions.py
+++ myfunctions.py
@@ -1,22 +1,41 @@
 import turtle
-bob=turtle.Turtle()
+import random
 
-#triangle
-def triangle():
-    for times in range(3):
-        bob.forward(100)
-        bob.left(120)
+def draw_triangle(turt, size, color1, color2, rotate_angle):  # Drawing a triangle
+    """
+    Draws a triangle, filling it with color.
+    Uses rotate_angle to offset
+    """
+    turt.fillcolor(color1)
+    turt.begin_fill()
+    for _ in range(3):
+        turt.forward(size)
+        turt.left(120)  # Each angle is 60 (180/3), the change of 120 makes this shape a triangle
+    turt.end_fill()
+    turt.right(rotate_angle) #rotates the turtle for an additional offset
 
-#square
-def square():
-    for times in range(4):
-           bob.forward(100)
-           bob.left(100)
-        
-#pentagon
-def pentagon():
-    for times in range(5):
-        bob.forward(100)
-        bob.left(72)
-        
-    
+def draw_diamond(turt, size, color1, color2, offset):
+  """
+    Draws a diamond made of 4 triangles
+  """
+  draw_triangle(turt, size, color1, color2, 60)
+  turt.forward(size)
+  draw_triangle(turt, size, color2, color1, 60)
+  turt.forward(size)
+  draw_triangle(turt, size, color1, color2, 60)
+  turt.forward(size)
+  draw_triangle(turt, size, color2, color1, 60)
+  turt.penup()
+  turt.goto(turt.xcor() - offset, turt.ycor() - offset)
+  turt.pendown()
+
+def set_up_screen(screen_color):
+  """
+    Sets up the screen.  Returns the turtle and the screen.
+  """
+  screen = turtle.Screen()
+  screen.bgcolor(screen_color)
+  t = turtle.Turtle()
+  t.speed(0)  # Fastest speed
+  t.hideturtle() # hides the turtle object
+  return t, screen
